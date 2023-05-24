a170cd7f8f618b9d7cb8e4ee7ebeb102ba01cea2
BreadMhmmmmm <134469225+BreadMhmmmmm@users.noreply.github.com>
2023-05-24 16:00:39 +1200

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents new and healthy dinner')
 
 streamlit.header('Healthy Breakfast menu ;)')
-streamlit.text('Strawberry Protein Pancakes')
-streamlit.text('Kale smoothie')
-streamlit.text('Eggs on toast')
-streamlit.text('Carror ommelete')
+streamlit.text('🥣Strawberry Protein Pancakes')
+streamlit.text(' 🥗 Kale smoothie')
+streamlit.text('🍞Eggs on toast')
+streamlit.text('🍞Carror ommelete')
