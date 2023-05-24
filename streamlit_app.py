df4dbc20f9fd3af78a695385911d0606785d66cc
BreadMhmmmmm <134469225+BreadMhmmmmm@users.noreply.github.com>
2023-05-24 15:58:28 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents new and healthy dinner')
+
+streamlit.header('Healthy Breakfast menu ;)')
+streamlit.text('Strawberry Protein Pancakes')
+streamlit.text('Kale smoothie')
+streamlit.text('Eggs on toast')
+streamlit.text('Carror ommelete')
